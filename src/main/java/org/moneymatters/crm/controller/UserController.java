486e7374d9f7832893144b4afd9fd19d431f9d3d
Anurag <anuragdubey05march@gmail.com>
2024-12-20 12:09:59 +0530

    Created Base File for CRM Project

--- src/main/java/org/moneymatters/crm/controller/UserController.java
+++ src/main/java/org/moneymatters/crm/controller/UserController.java
@@ -4,11 +4,20 @@ package org.moneymatters.crm.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String getGreeting(){
-        return "hello user this is a Greeting Message from the creator";
+    @PostMapping("/hello")
+    public String getGreeting(@RequestHeader("name") String name){
+        return "Hello "+name;
+    }
+    @PostMapping("/validate")
+    public String validateUserData(@RequestHeader("username") String username,@RequestHeader("password") String password, @RequestHeader("name") String name){
+        if(username.equals("Anurag_05") && password.equals("ad05"))
+            return ("Welcome " + name);
+        else
+            return "Invalid User Details";
     }
 }
