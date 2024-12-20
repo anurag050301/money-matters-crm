f17d9fdf7dfc10ca331de4d54a4e481fd5b8c9e1
Anurag <anuragdubey05march@gmail.com>
2024-12-20 15:49:34 +0530

    Created Base File for CRM Project

--- src/main/java/org/moneymatters/crm/controller/UserController.java
+++ src/main/java/org/moneymatters/crm/controller/UserController.java
@@ -1,7 +1,11 @@
 package org.moneymatters.crm.controller;
 
 
+import org.moneymatters.crm.dto.UserDTO;
+import org.moneymatters.crm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,15 +13,16 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @PostMapping("/hello")
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/hello")
     public String getGreeting(@RequestHeader("name") String name){
         return "Hello "+name;
     }
+
     @PostMapping("/validate")
-    public String validateUserData(@RequestHeader("username") String username,@RequestHeader("password") String password, @RequestHeader("name") String name){
-        if(username.equals("Anurag_05") && password.equals("ad05"))
-            return ("Welcome " + name);
-        else
-            return "Invalid User Details";
-    }
+    public ResponseEntity<String> validateUser(@RequestBody User user) {
+        String validationMessage = userService.validateUser(user);
+        return ResponseEntity.ok(validationMessage);
 }
---
+++ src/main/java/org/moneymatters/crm/service/UserService.java
@@ -0,0 +1,5 @@
+package org.moneymatters.crm.service;
+
+public class UserService {
+
+}
