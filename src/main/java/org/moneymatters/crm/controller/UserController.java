e62fbc2aa1e5123b3daed9586908838d79baa028
Anurag <anuragdubey05march@gmail.com>
2024-12-20 15:54:10 +0530

    User Validation

--- src/main/java/org/moneymatters/crm/controller/UserController.java
+++ src/main/java/org/moneymatters/crm/controller/UserController.java
@@ -1,14 +1,11 @@
 package org.moneymatters.crm.controller;
 
 
-import org.moneymatters.crm.dto.UserDTO;
+import org.moneymatters.crm.model.User;
 import org.moneymatters.crm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class UserController {
@@ -17,12 +14,13 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping("/hello")
-    public String getGreeting(@RequestHeader("name") String name){
-        return "Hello "+name;
+    public String getGreeting(@RequestHeader("name") String name) {
+        return "Hello " + name;
     }
 
     @PostMapping("/validate")
     public ResponseEntity<String> validateUser(@RequestBody User user) {
         String validationMessage = userService.validateUser(user);
         return ResponseEntity.ok(validationMessage);
+    }
 }
